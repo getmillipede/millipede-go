4fc4dffa26a5c615b9b6cec0134dec41cb69ce17
Manfred Touron <m@42.am>
2015-07-25 22:43:48 +0200

    Using codegangsta/cli for CLI parsing

---
+++ millipede-go.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strconv"
+
+	"github.com/codegangsta/cli"
+
+	millipede "github.com/getmillipede/millipede-go/millipede"
+	"github.com/getmillipede/millipede-go/version"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = path.Base(os.Args[0])
+	app.Author = "Millipede crew"
+	app.Email = "https://github.com/getmillipede/millipede-go"
+	app.Version = version.VERSION + " (" + version.GITCOMMIT + ")"
+	app.Usage = "Print a beautiful millipede"
+
+	app.Action = func(c *cli.Context) {
+		var size uint64
+		var err error
+		size = 20
+		if len(c.Args()) > 0 {
+			size, err = strconv.ParseUint(c.Args()[0], 10, 0)
+			if err != nil {
+				panic(err)
+			}
+		}
+		fmt.Println(millipede.Millipede(size))
+	}
+	app.Run(os.Args)
+}
